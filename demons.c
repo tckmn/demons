3b4d7cfac68a7505044881fa56959018a715e7f3
Andy Tockman <andy@tck.mn>
2025-04-09 12:56:04 -0400

    ability to print fairs (and say what to print)

--- demons.c
+++ demons.c
@@ -5,19 +5,27 @@
 #include <mpfr.h>
 #include <mpf2mpfr.h>
 
-/* #define MAXD 5 */
-/* #define MAXT 1000000 */
-
-#define MAXD 4
+#define MAXD 5
 #define MAXT 120000000
+
+#define PRINT_NOKILL
+#define PRINT_FAIRS
 
 #define P(d,t) probs[((d)-1)*(MAXD+1) + (((t)-1) % (MAXD+1))]
 
+
 int main(void) {
     mpf_set_default_prec(200);
+
+#ifdef PRINT_NOKILL
     int nokillmods[6] = {0, 2, 3, 12, 20, 60};
     int nokilldata[6*60];
     for (int i = 0; i < 6*60; ++i) nokilldata[i] = -1;
+#endif
+
+#ifdef PRINT_FAIRS
+    int fairs[6] = {0};
+#endif
 
     mpf_t probs[MAXD * (MAXD+1)];
     mpf_t dkill, tkill, nkill;
@@ -70,15 +78,32 @@ int main(void) {
             mpf_add(P(d,t), dkill, tkill);
             mpf_div_ui(P(d,t), P(d,t), d+t);
             if (mpf_cmp(P(d,t), nkill) < 0) {
+#ifdef PRINT_NOKILL
                 if (nokilldata[d*60 + (t%nokillmods[d])] == -1) {
                     nokilldata[d*60 + (t%nokillmods[d])] = t;
                     printf("nokill from d=%d t=%d (%d mod %d)\n", d, t, t%nokillmods[d], nokillmods[d]);
                 }
+#endif
                 mpf_set(P(d,t), nkill);
-            } else if (nokilldata[d*60 + (t%nokillmods[d])] != -1) {
+            }
+#ifdef PRINT_NOKILL
+            else if (nokilldata[d*60 + (t%nokillmods[d])] != -1) {
                 printf("nokill broken at d=%d t=%d (%d mod %d)\n", d, nokilldata[d*60 + (t%nokillmods[d])], t%nokillmods[d], nokillmods[d]);
                 nokilldata[d*60 + (t%nokillmods[d])] = -1;
             }
+#endif
+
+#ifdef PRINT_FAIRS
+            if (!fairs[d] && mpf_cmp_d(P(d,t), 0.5) > 0) {
+                fairs[d] = 1;
+                printf("P(d=%d,t=%d) = ", d, t-1);
+                mpf_out_str(stdout, 10, 0, P(d,t-1));
+                printf("\nP(d=%d,t=%d) = ", d, t);
+                mpf_out_str(stdout, 10, 0, P(d,t));
+                printf("\n");
+            }
+#endif
+
         }
     }
 
