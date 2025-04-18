51c22c6e2f0d71bb71fddf285620fffae19cbe4a
Andy Tockman <andy@tck.mn>
2025-04-18 11:18:46 -0400

    more demons

--- demons.c
+++ demons.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include <gmp.h>
 #include <mpfr.h>
 #include <mpf2mpfr.h>
 
-#define MAXD 5
+#define MAXD 15
 #define MAXT 120000000
 
 #define PRINT_NOKILL
@@ -13,6 +14,10 @@
 
 #define P(d,t) probs[((d)-1)*(MAXD+1) + (((t)-1) % (MAXD+1))]
 
+
+#if MAXD > 5
+#undef PRINT_NOKILL
+#endif
 
 int main(void) {
     mpf_set_default_prec(200);
@@ -24,7 +29,7 @@ int main(void) {
 #endif
 
 #ifdef PRINT_FAIRS
-    int fairs[6] = {0};
+    int fairs[MAXD+1] = {0};
 #endif
 
     mpf_t probs[MAXD * (MAXD+1)];
@@ -32,18 +37,40 @@ int main(void) {
     for (int i = 0; i < MAXD * (MAXD+1); ++i) mpf_init(probs[i]);
     mpf_init(dkill); mpf_init(tkill); mpf_init(nkill);
 
+    struct timeval init, prev, cur;
+    gettimeofday(&init, NULL); prev = init;
+
     for (int t = 1; t <= MAXT; ++t) {
-        if (t % 1000000 == 0) printf("at t=%d\n", t);
+        if (t % 1000000 == 0) {
+            gettimeofday(&cur, NULL);
+            unsigned long
+                initdiff = (cur.tv_sec - init.tv_sec) * 1000000 + cur.tv_usec - init.tv_usec,
+                prevdiff = (cur.tv_sec - prev.tv_sec) * 1000000 + cur.tv_usec - prev.tv_usec;
+            prev = cur;
+            printf("at t=%d (%lu.%06lus / %lu.%06lus)\n", t,
+                    prevdiff / 1000000, prevdiff % 1000000,
+                    initdiff / 1000000, initdiff % 1000000);
+        }
         for (int d = 1; d <= MAXD; ++d) {
             if (t - d + 1 <= d - 1) {
                 mpf_set_ui(dkill, 0);
             } else {
                 mpf_set_ui(dkill, 1);
                 switch (d) {
-                case 5: if (mpf_cmp(dkill, P(d-1,t-4)) > 0) mpf_set(dkill, P(d-1,t-4));
-                case 4: if (mpf_cmp(dkill, P(d-1,t-3)) > 0) mpf_set(dkill, P(d-1,t-3));
-                case 3: if (mpf_cmp(dkill, P(d-1,t-2)) > 0) mpf_set(dkill, P(d-1,t-2));
-                case 2: if (mpf_cmp(dkill, P(d-1,t-1)) > 0) mpf_set(dkill, P(d-1,t-1));
+                case 15: if (mpf_cmp(dkill, P(d-1,t-14)) > 0) mpf_set(dkill, P(d-1,t-14));
+                case 14: if (mpf_cmp(dkill, P(d-1,t-13)) > 0) mpf_set(dkill, P(d-1,t-13));
+                case 13: if (mpf_cmp(dkill, P(d-1,t-12)) > 0) mpf_set(dkill, P(d-1,t-12));
+                case 12: if (mpf_cmp(dkill, P(d-1,t-11)) > 0) mpf_set(dkill, P(d-1,t-11));
+                case 11: if (mpf_cmp(dkill, P(d-1,t-10)) > 0) mpf_set(dkill, P(d-1,t-10));
+                case 10: if (mpf_cmp(dkill, P(d-1,t-9))  > 0) mpf_set(dkill, P(d-1,t-9));
+                case 9:  if (mpf_cmp(dkill, P(d-1,t-8))  > 0) mpf_set(dkill, P(d-1,t-8));
+                case 8:  if (mpf_cmp(dkill, P(d-1,t-7))  > 0) mpf_set(dkill, P(d-1,t-7));
+                case 7:  if (mpf_cmp(dkill, P(d-1,t-6))  > 0) mpf_set(dkill, P(d-1,t-6));
+                case 6:  if (mpf_cmp(dkill, P(d-1,t-5))  > 0) mpf_set(dkill, P(d-1,t-5));
+                case 5:  if (mpf_cmp(dkill, P(d-1,t-4))  > 0) mpf_set(dkill, P(d-1,t-4));
+                case 4:  if (mpf_cmp(dkill, P(d-1,t-3))  > 0) mpf_set(dkill, P(d-1,t-3));
+                case 3:  if (mpf_cmp(dkill, P(d-1,t-2))  > 0) mpf_set(dkill, P(d-1,t-2));
+                case 2:  if (mpf_cmp(dkill, P(d-1,t-1))  > 0) mpf_set(dkill, P(d-1,t-1));
                 }
             }
             mpf_mul_ui(dkill, dkill, d);
@@ -53,11 +80,21 @@ int main(void) {
             } else {
                 mpf_set_ui(tkill, 1);
                 switch (d) {
-                case 5: if (mpf_cmp(tkill, P(d,t-6)) > 0) mpf_set(tkill, P(d,t-6));
-                case 4: if (mpf_cmp(tkill, P(d,t-5)) > 0) mpf_set(tkill, P(d,t-5));
-                case 3: if (mpf_cmp(tkill, P(d,t-4)) > 0) mpf_set(tkill, P(d,t-4));
-                case 2: if (mpf_cmp(tkill, P(d,t-3)) > 0) mpf_set(tkill, P(d,t-3));
-                case 1: if (mpf_cmp(tkill, P(d,t-2)) > 0) mpf_set(tkill, P(d,t-2));
+                case 15: if (mpf_cmp(tkill, P(d,t-16)) > 0) mpf_set(tkill, P(d,t-16));
+                case 14: if (mpf_cmp(tkill, P(d,t-15)) > 0) mpf_set(tkill, P(d,t-15));
+                case 13: if (mpf_cmp(tkill, P(d,t-14)) > 0) mpf_set(tkill, P(d,t-14));
+                case 12: if (mpf_cmp(tkill, P(d,t-13)) > 0) mpf_set(tkill, P(d,t-13));
+                case 11: if (mpf_cmp(tkill, P(d,t-12)) > 0) mpf_set(tkill, P(d,t-12));
+                case 10: if (mpf_cmp(tkill, P(d,t-11)) > 0) mpf_set(tkill, P(d,t-11));
+                case 9:  if (mpf_cmp(tkill, P(d,t-10)) > 0) mpf_set(tkill, P(d,t-10));
+                case 8:  if (mpf_cmp(tkill, P(d,t-9))  > 0) mpf_set(tkill, P(d,t-9));
+                case 7:  if (mpf_cmp(tkill, P(d,t-8))  > 0) mpf_set(tkill, P(d,t-8));
+                case 6:  if (mpf_cmp(tkill, P(d,t-7))  > 0) mpf_set(tkill, P(d,t-7));
+                case 5:  if (mpf_cmp(tkill, P(d,t-6))  > 0) mpf_set(tkill, P(d,t-6));
+                case 4:  if (mpf_cmp(tkill, P(d,t-5))  > 0) mpf_set(tkill, P(d,t-5));
+                case 3:  if (mpf_cmp(tkill, P(d,t-4))  > 0) mpf_set(tkill, P(d,t-4));
+                case 2:  if (mpf_cmp(tkill, P(d,t-3))  > 0) mpf_set(tkill, P(d,t-3));
+                case 1:  if (mpf_cmp(tkill, P(d,t-2))  > 0) mpf_set(tkill, P(d,t-2));
                 }
             }
             mpf_mul_ui(tkill, tkill, t);
@@ -67,11 +104,21 @@ int main(void) {
             } else {
                 mpf_set_ui(nkill, 1);
                 switch (d) {
-                case 5: if (mpf_cmp(nkill, P(d,t-5)) > 0) mpf_set(nkill, P(d,t-5));
-                case 4: if (mpf_cmp(nkill, P(d,t-4)) > 0) mpf_set(nkill, P(d,t-4));
-                case 3: if (mpf_cmp(nkill, P(d,t-3)) > 0) mpf_set(nkill, P(d,t-3));
-                case 2: if (mpf_cmp(nkill, P(d,t-2)) > 0) mpf_set(nkill, P(d,t-2));
-                case 1: if (mpf_cmp(nkill, P(d,t-1)) > 0) mpf_set(nkill, P(d,t-1));
+                case 15: if (mpf_cmp(nkill, P(d,t-15)) > 0) mpf_set(nkill, P(d,t-15));
+                case 14: if (mpf_cmp(nkill, P(d,t-14)) > 0) mpf_set(nkill, P(d,t-14));
+                case 13: if (mpf_cmp(nkill, P(d,t-13)) > 0) mpf_set(nkill, P(d,t-13));
+                case 12: if (mpf_cmp(nkill, P(d,t-12)) > 0) mpf_set(nkill, P(d,t-12));
+                case 11: if (mpf_cmp(nkill, P(d,t-11)) > 0) mpf_set(nkill, P(d,t-11));
+                case 10: if (mpf_cmp(nkill, P(d,t-10)) > 0) mpf_set(nkill, P(d,t-10));
+                case 9:  if (mpf_cmp(nkill, P(d,t-9))  > 0) mpf_set(nkill, P(d,t-9));
+                case 8:  if (mpf_cmp(nkill, P(d,t-8))  > 0) mpf_set(nkill, P(d,t-8));
+                case 7:  if (mpf_cmp(nkill, P(d,t-7))  > 0) mpf_set(nkill, P(d,t-7));
+                case 6:  if (mpf_cmp(nkill, P(d,t-6))  > 0) mpf_set(nkill, P(d,t-6));
+                case 5:  if (mpf_cmp(nkill, P(d,t-5))  > 0) mpf_set(nkill, P(d,t-5));
+                case 4:  if (mpf_cmp(nkill, P(d,t-4))  > 0) mpf_set(nkill, P(d,t-4));
+                case 3:  if (mpf_cmp(nkill, P(d,t-3))  > 0) mpf_set(nkill, P(d,t-3));
+                case 2:  if (mpf_cmp(nkill, P(d,t-2))  > 0) mpf_set(nkill, P(d,t-2));
+                case 1:  if (mpf_cmp(nkill, P(d,t-1))  > 0) mpf_set(nkill, P(d,t-1));
                 }
             }
 
